e424a214e9d24c2a0e6847767c3e9c3abe45b9c9
Pedro <pedroaugustosilva.880>
2023-07-26 16:15:54 -0300

    variable ambient

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,7 +4,7 @@ import { parseCookies } from "nookies";
 const { "finance.token": token } = parseCookies();
 
 export const api = axios.create({
-  baseURL: "https://finance-6bf7.vercel.app/",
+  baseURL: "https://finance-6bf7.vercel.app/api",
 });
 
 if (token) {
